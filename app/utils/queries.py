87c974b7c1b27f5118bfea268734d8eb065559ec
Sophie O'Hagan <sophie.ohagan@version1.com>
2024-03-25 13:59:15 +0000

    Updated queries for flattened fee reduction

--- app/utils/queries.py
+++ app/utils/queries.py
@@ -12,8 +12,8 @@ def connect(connection_string):
 
 
 def insert_metric(conn, idx, metric):
-    sql = """INSERT INTO metrics(uuid, application_type, fee, fee_reduction, applicant_type, latitude, longitude, local_authority_district, region, property_type, project_type, submission_date)
-          VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    sql = """INSERT INTO metrics(uuid, application_type, fee, fee_reduction_sports, fee_reduction_parish_council, fee_reduction_alternative, applicant_type, latitude, longitude, local_authority_district, region, property_type, project_type, submission_date)
+          VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
     try:
         with conn.cursor() as cur:
             cur.execute(sql, metric.sql_string(idx))
